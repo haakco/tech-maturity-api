83a51552379b280daf1df136e8bcad8e07c10a65
Tim Haak <tim@haak.co>
2017-08-08 13:48:01 +0200

    Ignore db files. Create db if it doesnt exist

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,21 @@
 import bluebird from 'bluebird';
 import levelup from 'levelup';
+import initialiseDB from './initialise-db';
 
 const dbPath = `${__dirname}/techdb.db`;
 
 const db = bluebird.promisifyAll(levelup(dbPath, {
-  createIfMissing: false,
+  createIfMissing: true,
   valueEncoding: {
     encode: JSON.stringify,
     decode: JSON.parse,
   },
 }));
+
+db.getAsync('version')
+  .then()
+  .catch(() => {
+    initialiseDB(db);
+  });
 
 export default db;
--- src/database/initialise-db.js
+++ src/database/initialise-db.js
@@ -1,21 +1,14 @@
-import bluebird from 'bluebird';
-import levelup from 'levelup';
 import allData from './migrations/allData';
 
-const dbPath = `${__dirname}/techdb.db`;
+function initialiseDB(db) {
+  db
+    .putAsync('asset_types', allData.asset_types)
+    .then(() => db.putAsync('version', 1))
+    .then(() => db.putAsync('categories', allData.categories))
+    .then(() => console.log('done'))
+    .catch((e) => {
+      console.error(e);
+    });
+}
 
-const db = bluebird.promisifyAll(levelup(dbPath, {
-  createIfMissing: true,
-  valueEncoding: {
-    encode: JSON.stringify,
-    decode: JSON.parse,
-  },
-}));
-
-db
-  .putAsync('asset_types', allData.asset_types)
-  .then(() => db.putAsync('categories', allData.categories))
-  .then(() => console.log('done'))
-  .catch((e) => {
-    console.error(e);
-  });
+export default initialiseDB;
